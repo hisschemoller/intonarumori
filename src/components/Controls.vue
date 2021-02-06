d0659b40913f02e79a9c61643a743639b2af0e16
Wouter Hisschemöller <email@hisschemoller.com>
2021-02-06 12:24:29 +0100

    Controls component uses store.

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -1,5 +1,40 @@
 <template>
   <div id="controls">
-    <button type="button" id="controls__settings"><span class="icon-cog"></span></button>
+    <button type="button" @click="toggleSettings(true)"><span class="icon-cog"></span></button>
   </div>
 </template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+import { mapActions } from 'vuex';
+import { ActionTypes } from '../store/actions';
+
+export default defineComponent({
+  methods: {
+    ...mapActions({
+      toggleSettings: ActionTypes.ToggleSettings,
+    }),
+  },
+});
+
+</script>
+
+<style scoped>
+#controls {
+  right: 0;
+  position: absolute;
+  top: 0;
+}
+#controls button {
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  font-size: 24px;
+  outline: none;
+  padding: 10px;
+  position: absolute;
+  right: 20px;
+  top: 20px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import store from './store';
+import { store } from './store';
 import './assets/css/style.css';
 
 createApp(App).use(store).mount('#app');
