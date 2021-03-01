93af6bbed232276d18390dde4f170a3e2fa61743
Wouter Hisschemöller <email@hisschemoller.com>
2021-03-01 15:37:36 +0100

    Set HTML page title.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   publicPath: '/intonarumori/',
+  chainWebpack: (config) => {
+    config
+      .plugin('html')
+      .tap((args) => {
+        // eslint-disable-next-line no-param-reassign
+        args[0].title = 'Intonarumori 1.0';
+        return args;
+      });
+  },
 };
