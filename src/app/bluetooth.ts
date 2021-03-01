43a662d3fe2a01cea30c74a7cd518845db7f7610
Wouter Hisschemöller <email@hisschemoller.com>
2021-03-01 12:22:29 +0100

    Commit Bluetooth MIDI messages.

--- src/app/bluetooth.ts
+++ src/app/bluetooth.ts
@@ -1,11 +1,14 @@
+import { Store } from 'vuex';
 import { useStore } from '../store';
 import { MutationType } from '../store/mutations';
+import { State } from '../store/state';
 
 const BLUETOOTH_SERVICE_UUID = '03b80e5a-ede8-4b33-a751-6ce34ec4c700';
 let device: BluetoothDevice;
 let server: BluetoothRemoteGATTServer;
 let service: BluetoothRemoteGATTService;
 let characteristic: BluetoothRemoteGATTCharacteristic;
+let store: Store<State>;
 
 export const enum BluetoothStatus {
   Idle,
@@ -22,14 +25,22 @@ export const enum BluetoothStatus {
 function onCharacteristicValueChanged(e: Event) {
   const { value } = e.target as BluetoothRemoteGATTCharacteristic;
   if (typeof value !== 'undefined') {
-    console.log(
+    // console.log(
+    //   // eslint-disable-next-line no-bitwise
+    //   value.getUint8(2) & 0xf0,
+    //   // eslint-disable-next-line no-bitwise
+    //   value.getUint8(2) & 0x0f,
+    //   value.getUint8(3),
+    //   value.getUint8(4),
+    // );
+    store.commit(MutationType.HandleMIDIMessage, {
       // eslint-disable-next-line no-bitwise
-      value.getUint8(2) & 0xf0,
+      type: value.getUint8(2) & 0xf0,
       // eslint-disable-next-line no-bitwise
-      value.getUint8(2) & 0x0f,
-      value.getUint8(3),
-      value.getUint8(4),
-    );
+      channel: value.getUint8(2) & 0x0f,
+      data0: value.getUint8(3),
+      data1: value.getUint8(4),
+    });
   }
 }
 
@@ -37,7 +48,7 @@ function onCharacteristicValueChanged(e: Event) {
  * Bluetooth device scan, connect and subscribe.
  */
 export async function connect() {
-  const store = useStore();
+  store = useStore();
   const options = {
     filters: [{
       services: [BLUETOOTH_SERVICE_UUID],
