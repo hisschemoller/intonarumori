e8f0a84edcc0fd3f12d0d61404d4d886107e667b
Wouter Hisschemöller <email@hisschemoller.com>
2021-04-03 17:35:28 +0200

    Playable with number keyboard keys.

--- src/components/Pads.vue
+++ src/components/Pads.vue
@@ -18,6 +18,25 @@ import { MutationType } from '../store/mutations';
 export default defineComponent({
   setup() {
     const store = useStore();
+    const numPads = store.state.bumpers.length;
+
+    document.addEventListener('keydown', (e) => {
+      const padIndex: number = e.key.charCodeAt(0) - 49;
+      if (padIndex >= 0 && padIndex < numPads) {
+        store.commit(
+          MutationType.TriggerNote, { padIndex, velocity: 100 },
+        );
+      }
+    });
+
+    document.addEventListener('keyup', (e) => {
+      const padIndex: number = e.key.charCodeAt(0) - 49;
+      if (padIndex >= 0 && padIndex < numPads) {
+        store.commit(
+          MutationType.TriggerNote, { padIndex, velocity: 0 },
+        );
+      }
+    });
 
     const onMouseEvent = (padIndex: number, velocity: number) => {
       store.commit(
