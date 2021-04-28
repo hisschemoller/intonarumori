8e17c356bdc3709b2b47220cf28054f3784c7ae6
Wouter Hisschemöller <email@hisschemoller.com>
2021-04-28 23:17:04 +0200

    Four identical flags.

--- src/enable3d/enable3d.ts
+++ src/enable3d/enable3d.ts
@@ -5,9 +5,9 @@ import {
 let rootEl: HTMLDivElement;
 
 class MainScene extends Scene3D {
-  private box!: ExtendedObject3D;
+  private fix!: ExtendedObject3D;
 
-  private pole!: ExtendedObject3D;
+  private flags: ExtendedObject3D[] = [];
 
   constructor() {
     super({ key: 'MainScene' });
@@ -32,59 +32,42 @@ class MainScene extends Scene3D {
     //   this.physics.debug.enable();
     // }
 
-    // position camera
     this.camera.position.set(10, 10, 20);
 
-    // blue box (without physics)
-    // this.box = this.add.box({ y: 2 }, { lambert: { color: 'deepskyblue' } });
-
-    // pink box (with physics)
-    // this.physics.add.box({ y: 10 }, { lambert: { color: 'hotpink' } });
-    // this.physics.add.cylinder({ x: 0.1, y: 15 }, { lambert: { color: 'blue' } });
-
-    // const hinge = (x) => {
-
-    //   this.physics.add.constraints.hinge(box1.body, box2.body, {
-    //     pivotA: { y: -0.65 },
-    //     pivotB: { y: 0.65 },
-    //     axisA: { x: 1 },
-    //     axisB: { x: 1 }
-    //   })
-    //   this.physics.add.constraints.hinge(box2.body, box3.body, {
-    //     pivotA: { y: -0.65 },
-    //     pivotB: { y: 0.65 },
-    //     axisA: { x: 1 },
-    //     axisB: { x: 1 }
-    //   })
-    // }
-
-    const fix = this.physics.add.box({
-      x: 0, y: 0.5, z: 0, mass: 0,
+    this.fix = this.physics.add.box({
+      x: 0, y: -0.05, z: 0, mass: 0, width: 0.1, height: 0.1, depth: 0.1,
     }, { lambert: { color: 'blue' } });
 
-    this.pole = this.add.cylinder({
-      height: 4, radiusBottom: 0.05, radiusTop: 0.05, y: 3, mass: 0.1,
+    this.flags.push(this.createFlag(0, 0));
+    this.flags.push(this.createFlag(-2, 1));
+    this.flags.push(this.createFlag(1, 2));
+    this.flags.push(this.createFlag(1, -1));
+  }
+
+  createFlag(x: number, z: number) {
+    const pole = this.add.cylinder({
+      height: 4, radiusBottom: 0.05, radiusTop: 0.05, x, y: 2, z, mass: 0.1,
     }, { lambert: { color: 'white' } });
 
     const flag = this.add.box({
       depth: 0.1, x: 0.55, y: 1.5,
     }, { lambert: { color: 'red' } });
 
-    this.pole.add(flag);
-    this.physics.add.existing(this.pole);
+    pole.add(flag);
+    this.physics.add.existing(pole);
 
-    this.physics.add.constraints.hinge(fix.body, this.pole.body, {
-      pivotA: { x: 0, y: 0.5, z: 0 },
-      pivotB: { x: 0, y: -2, z: 0 },
+    this.physics.add.constraints.hinge(this.fix.body, pole.body, {
+      pivotA: { x, y: 0.05, z },
+      pivotB: { y: -2 },
       axisA: { y: 1 },
       axisB: { y: 1 },
     });
-    // this.pole.body.setFriction(0.95);
-    this.pole.body.ammo.setDamping(0.99, 0.99);
+    pole.body.ammo.setDamping(0.99, 0.99);
+    return pole;
   }
 
   update() {
-    this.pole.body.applyTorque(0, -0.5, 0);
+    this.flags.forEach((flag) => flag.body.applyTorque(0, 0.5, 0));
   }
 }
 
@@ -95,7 +78,6 @@ export default function setup(
   rootElm: HTMLDivElement,
 ): void {
   rootEl = rootElm;
-  console.log('setup');
   // set your project configs
   const config = { scenes: [MainScene] };
   // load the ammo.js file from the /lib folder and start the project
